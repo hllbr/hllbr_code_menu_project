d298a75df9e8365500059f18513780dade59fcdc
hllbr <halibrahim.kocak@gmail.com>
2022-01-23 17:26:27 +0300

    trying

---
+++ app.js
@@ -0,0 +1,154 @@
+const menu = [
+  {
+    id: 1,
+    title: "JAVASCRIPT",
+    category: "LEARNING",
+    price: 60000,
+    img: "./images/item-1.jpeg",
+    desc: `JUST DO IT`,
+  },
+  {
+    id: 2,
+    title: "JAVA",
+    category: "LEARNED",
+    price: 13.99,
+    img: "./images/item-2.jpeg",
+    desc: `WONDERFUL`,
+  },
+  {
+    id: 3,
+    title: "NODE JS",
+    category: "LEARNING",
+    price: 6.99,
+    img: "./images/item-3.jpeg",
+    desc: `JUST DO IT`,
+  },
+  {
+    id: 4,
+    title: "PYTHON",
+    category: "LEARNED",
+    price: 20.99,
+    img: "./images/item-4.jpeg",
+    desc: `SUCCESSFUL`,
+  },
+  {
+    id: 5,
+    title: "REACT",
+    category: "LEARNING",
+    price: 22.99,
+    img: "./images/item-5.jpeg",
+    desc: `? `,
+  },
+  {
+    id: 6,
+    title: "CSS",
+    category: "unknow",
+    price: 18.99,
+    img: "./images/item-6.jpeg",
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: "HTML",
+    category: "LEARNING",
+    price: 8.99,
+    img: "./images/item-7.jpeg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: "DB RELATIONSHIP",
+    category: "LEARNED",
+    price: 12.99,
+    img: "./images/item-8.jpeg",
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  },
+  {
+    id: 9,
+    title: "C",
+    category: "LEARNED",
+    price: 16.99,
+    img: "./images/item-9.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 10,
+    title: "C++",
+    category: "LEARNED",
+    price: 22.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+ 
+];
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {
+  diplayMenuItems(menu);
+  displayMenuButtons();
+});
+
+function diplayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
+
+    return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">${item.price}M €</h4>
+            </header>
+            <p class="item-text">
+             
+            </p>
+          </div>
+        </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  // console.log(displayMenu);
+
+  sectionCenter.innerHTML = displayMenu;
+}
+function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button type="button" class="filter-btn" data-id=${category}>
+          ${category}
+        </button>`;
+    })
+    .join("");
+
+  btnContainer.innerHTML = categoryBtns;
+  const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+  console.log(filterBtns);
+
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      // console.log(e.currentTarget.dataset);
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        // console.log(menuItem.category);
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "all") {
+        diplayMenuItems(menu);
+      } else {
+        diplayMenuItems(menuCategory);
+      }
+    });
+  });
+}
